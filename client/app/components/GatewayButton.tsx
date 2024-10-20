82d6d73849b6971210227ebb7f253e9db15c260f
Florian Trayon <git@florian-trayon.fr>
2024-10-20 20:04:42 +0200

    Moved dashboard button code to a dedicated React component

---
+++ client/app/components/GatewayButton.tsx
@@ -0,0 +1,46 @@
+//
+// Composant du bouton pour accéder au tableau de bord.
+//
+
+"use client";
+
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+import { ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function GatewayButton()
+{
+	// Déclaration des variables d'état.
+	const router = useRouter();
+	const [ isLoading, setIsLoading ] = useState( false );
+
+	// Simulation d'une requête HTTP au serveur.
+	useEffect( () =>
+	{
+		router.prefetch( "/dashboard" );
+
+		if ( isLoading )
+		{
+			setTimeout( () =>
+			{
+				router.push( "/dashboard" );
+				setIsLoading( false );
+			}, 3000 );
+		}
+	}, [ router, isLoading ] );
+
+	// Affichage du rendu HTML du composant.
+	return (
+		<Button
+			size="lg"
+			color="success"
+			onClick={() => setIsLoading( true )}
+			variant="shadow"
+			isLoading={isLoading}
+			endContent={<ArrowRight width={20} height={20} />}
+		>
+			Allons-y&nbsp;!
+		</Button>
+	);
+}
