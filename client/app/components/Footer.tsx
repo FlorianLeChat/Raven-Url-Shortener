b57d5a1c9760e860268789607729a563bc482f73
Florian Trayon <git@florian-trayon.fr>
2024-11-26 21:39:41 +0100

    Fixed ESLint import and dependency resolution in front-end folder

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -11,17 +11,17 @@ export default function Footer()
 	// Affichage du rendu HTML du composant.
 	return (
 		<footer className="container mx-auto mt-auto max-w-[1440px] p-4 !pt-0 md:p-8">
-			<p className="text-default-400 text-sm">
+			<p className="text-sm text-default-400">
 				© {new Date().getFullYear()} Raven Url Shortener. Tous droits
 				réservés.
 			</p>
 
-			<small className="text-default-400 text-sm">
+			<small className="text-sm text-default-400">
 				Ce site est protégé par reCAPTCHA sur lequel s&lsquo;appliquent
 				les{" "}
 				<Link
 					href="https://policies.google.com/privacy"
-					className="text-warning text-sm"
+					className="text-sm text-warning"
 					isExternal
 					showAnchorIcon
 				>
@@ -30,7 +30,7 @@ export default function Footer()
 				et les{" "}
 				<Link
 					href="https://policies.google.com/terms"
-					className="text-warning text-sm"
+					className="text-sm text-warning"
 					isExternal
 					showAnchorIcon
 				>
