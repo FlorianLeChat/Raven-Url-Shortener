bdabed86e80e3a07d2a155891663c94378ce7e08
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2024-12-09 20:32:11 +0100

    Added missing TailwindCSS animations for GitHub animated icon

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,7 +2,20 @@ import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 export default {
-	theme: {},
+	theme: {
+		extend: {
+			keyframes: {
+				github: {
+					"0%, 100%": { transform: "rotate(0)" },
+					"20%, 60%": { transform: "rotate(-25deg)" },
+					"40%, 80%": { transform: "rotate(10deg)" }
+				}
+			},
+			animation: {
+				github: "github 560ms ease-in-out"
+			}
+		}
+	},
 	content: [
 		"./app/**/*.{ts,tsx}",
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,jsx,ts,tsx,mdx}"
