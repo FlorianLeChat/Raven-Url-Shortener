05a502655a481cb6fb9875ee6a33883e441419cb
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-01-05 17:01:55 +0100

    feat: added final version of shortcut link summary page

--- client/app/[locale]/dashboard/components/summary-container.tsx
+++ client/app/[locale]/dashboard/components/summary-container.tsx
@@ -5,13 +5,20 @@
 "use client";
 
 import NextImage from "next/image";
+import { useContext } from "react";
 import { CircleCheckBig } from "lucide-react";
 import { Card, Image, Snippet, CardBody, CardHeader } from "@nextui-org/react";
+
+import SummaryActions from "./summary-actions";
+import { ServerContext } from "../../components/server-provider";
 
 export default function SummaryContainer( {
 	qrCode
 }: Readonly<{ qrCode: string }> )
 {
+	// Déclaration des variables d'état.
+	const serverData = useContext( ServerContext );
+
 	// Affichage du rendu HTML du composant.
 	return (
 		<Card
@@ -24,42 +31,45 @@ export default function SummaryContainer( {
 				as="header"
 				className="gap-3 bg-success-700 p-4 text-white dark:bg-success-200"
 			>
-				{/* Astuce d'utilisation */}
+				{/* Date de création */}
 				<CircleCheckBig className="inline-block min-w-[24px]" />
 				Votre lien a été créé le 23 septembre 2021 à 14:37:42.
 			</CardHeader>
 
-			<CardBody className="gap-5 p-4">
-				<div className="flex flex-row gap-5">
-					<Image
-						as={NextImage}
-						src={qrCode}
-						alt="NextUI hero Image"
-						width={200}
-						height={200}
-					/>
+			<CardBody className="flex-col gap-5 p-4 xl:flex-row">
+				<Image
+					as={NextImage}
+					src={qrCode}
+					alt="Code QR de votre lien raccourci"
+					width={200}
+					height={200}
+					className="max-h-[200px] min-h-[200px] min-w-[200px] max-w-[200px]"
+				/>
 
-					<div className="flex flex-col gap-3">
-						<h3 className="text-xl font-semibold">
-							Liens à votre disposition
-						</h3>
+				{/* Liens de partage */}
+				<div className="flex max-w-full flex-col gap-3">
+					<h3 className="text-xl font-semibold">
+						Liens à votre disposition
+					</h3>
 
-						<Snippet size="lg">
-							https://url.florian-dev.fr/1A7hby
-						</Snippet>
+					<Snippet size="lg" className="overflow-auto" hideSymbol>
+						{`${ serverData?.domain }1A7hby`}
+					</Snippet>
 
-						<Snippet size="lg">
-							https://url.florian-dev.fr/dffbd949-5144-477f-b94e-f6b63ec5b412
-						</Snippet>
+					<Snippet size="lg" className="overflow-auto" hideSymbol>
+						{`${ serverData?.domain }dffbd949-5144-477f-b94e-f6b63ec5b412`}
+					</Snippet>
 
-						<p className="text-sm text-default-500">
-							Ces liens Internet sont disponibles pour toute la
-							durée de vie de votre lien raccourci.
-							<br />
-							Vous pouvez les utiliser sans restriction.
-						</p>
-					</div>
+					<p className="text-sm text-default-500">
+						Ces liens Internet sont disponibles pour toute la durée
+						de vie de votre lien raccourci.
+						<br />
+						Vous pouvez les utiliser sans restriction.
+					</p>
 				</div>
+
+				{/* Actions rapides */}
+				<SummaryActions />
 			</CardBody>
 		</Card>
 	);
