c4fbbb7a87f87bf0b68e4ba011336f880640dbf2
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-01-05 16:52:53 +0100

    feat: remove fake loading of gateway button on home page

--- client/app/[locale]/components/gateway-button.tsx
+++ client/app/[locale]/components/gateway-button.tsx
@@ -7,37 +7,19 @@
 import { Button } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { ArrowRight } from "lucide-react";
-import { useEffect, useState } from "react";
 
 export default function GatewayButton()
 {
 	// Déclaration des variables d'état.
 	const router = useRouter();
-	const [ isLoading, setIsLoading ] = useState( false );
-
-	// Simulation d'une requête HTTP au serveur.
-	useEffect( () =>
-	{
-		router.prefetch( "/dashboard" );
-
-		if ( isLoading )
-		{
-			setTimeout( () =>
-			{
-				router.push( "/dashboard" );
-				setIsLoading( false );
-			}, 3000 );
-		}
-	}, [ router, isLoading ] );
 
 	// Affichage du rendu HTML du composant.
 	return (
 		<Button
 			size="lg"
 			color="success"
-			onPress={() => setIsLoading( true )}
+			onPress={() => router.push( "/dashboard" )}
 			variant="shadow"
-			isLoading={isLoading}
 			endContent={<ArrowRight width={20} height={20} />}
 		>
 			Allons-y&nbsp;!
