18a8d3d06f7a79a1c27fb9b10b85430967ace2e8
Florian Trayon <git@florian-trayon.fr>
2025-01-18 20:45:03 +0100

    feat: added NextJS server action to create shortcut link to PHP back-end

---
+++ client/app/[locale]/dashboard/actions/create-link.ts
@@ -0,0 +1,72 @@
+//
+// Action de création d'un nouveau raccourci auprès du back-end PHP.
+//
+
+"use server";
+
+import { logger } from "@/utilities/pino";
+
+export async function createLink( data: FormData )
+{
+	// Suppression de toutes les entrées vides.
+	data.forEach( ( value, key ) =>
+	{
+		if ( !value )
+		{
+			data.delete( key );
+		}
+	} );
+
+	logger.debug(
+		{ source: __dirname, data },
+		"Form data before sending to the server."
+	);
+
+	// Envoi de la requête HTTP de création d'un nouveau raccourci.
+	const response = await fetch( "http://localhost:8000/api/link", {
+		body: data,
+		method: "POST"
+	} );
+
+	try
+	{
+		const json = ( await response.json() ) as {
+			id?: string;
+			message?: string;
+		};
+
+		logger.info(
+			{ source: __dirname, json },
+			"Short link creation response."
+		);
+
+		if ( response.ok )
+		{
+			// Tout s'est bien passé.
+			return {
+				state: true,
+				data: json.id
+			};
+		}
+
+		// En cas d'erreur lors de la création du raccourci,
+		return {
+			state: false,
+			message: json.message ?? "Une erreur est survenue lors de la création du raccourci."
+		};
+	}
+	catch ( error )
+	{
+		// En cas d'erreur lors de la création du raccourci,
+		//  ou lors de la conversion de la réponse en JSON.
+		logger.error(
+			{ source: __dirname, error },
+			"An error occurred while creating the short link."
+		);
+
+		return {
+			state: false,
+			message: "Une erreur est survenue lors de la création du raccourci."
+		};
+	}
+}
