2dc321c7510206607d14423111c74001fb5d82b7
Florian Trayon <git@florian-trayon.fr>
2025-01-18 20:32:22 +0100

    feat: added JSON data from created entity to PHP back-end response

--- server/src/Action/CreateLinkAction.php
+++ server/src/Action/CreateLinkAction.php
@@ -39,6 +39,6 @@ final class CreateLinkAction extends AbstractController
 
 		$link = $service->createLink($request);
 
-		return new JsonResponse($link, JsonResponse::HTTP_CREATED);
+		return new JsonResponse($link->toArray(), JsonResponse::HTTP_CREATED);
 	}
 }
