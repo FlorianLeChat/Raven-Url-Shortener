096f52bdb2ebab85eafc7ab3caefe7d490600d33
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-01-19 14:26:53 +0100

    feat: added PHP route and server action to fetch link details

---
+++ server/src/Action/GetLinkDetailsAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Action;
+
+use App\Domain\Entity\Link;
+use Psr\Log\LoggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use const App\LOG_FUNCTION;
+
+/**
+ * Action pour la récupération des informations d'un lien raccourci.
+ */
+final class GetLinkDetailsAction extends AbstractController
+{
+	/**
+	 * Constructeur de la classe.
+	 */
+	public function __construct(
+		private readonly LoggerInterface $logger,
+		private readonly ValidatorInterface $validator,
+		private readonly EntityManagerInterface $entityManager
+	) {}
+
+	/**
+	 * Récupération des informations d'un lien raccourci.
+	 */
+	#[Route("/api/link/{uuid}", methods: ["GET"], stateless: true)]
+	public function getLinkDetails(#[MapEntity(id: "uuid")] Link $link): JsonResponse
+	{
+		$this->logger->info(sprintf(LOG_FUNCTION, basename(__FILE__), __NAMESPACE__, __FUNCTION__, __LINE__));
+
+		return new JsonResponse($link->toArray(), JsonResponse::HTTP_OK);
+	}
+}
