cb964f799b4fb31963b9d1acb5b196cb3cf5a4cf
Florian Trayon <git@florian-trayon.fr>
2025-01-19 20:02:08 +0100

    refactor: moved HTTP response interface for reCAPTCHA validation

--- client/interfaces/RecaptchaValidation.ts
+++ client/interfaces/RecaptchaValidation.ts
@@ -2,7 +2,7 @@
 // Interface des attributs des réponses de l'API de Google reCAPTCHA.
 //  Source : https://developers.google.com/recaptcha/docs/v3
 //
-export interface RecaptchaResponse {
+export interface RecaptchaValidation {
 	// État de validité du jeton d'authentification.
 	success: boolean;
 
