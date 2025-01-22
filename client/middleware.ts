95749163230f1101adb95639ef202003285aea24
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-01-22 20:42:05 +0100

    feat: added shortcut link routing to a redirect page on the NextJS side

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,14 +1,35 @@
 //
 // Mécanisme de routage pour les pages de l'application.
 //
-import { NextRequest } from "next/server";
 import createIntlMiddleware from "next-intl/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
 import { getLanguages } from "./utilities/i18n";
+import { getLinkDetails } from "./app/[locale]/dashboard/actions/get-link-details";
 
 export default async function middleware( request: NextRequest )
 {
-	// On créé le mécanisme de gestion des langues et traductions.
+	// Vérification à l'accès d'un lien raccourci.
+	const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+	const slugPattern = /^[a-zA-Z0-9-]+$/;
+
+	const pathName = request.nextUrl.pathname.slice( 1 );
+	const isValidUuid = uuidPattern.test( pathName );
+	const isMaybeSlug = pathName.length <= 50 && slugPattern.test( pathName );
+
+	if ( isValidUuid || isMaybeSlug )
+	{
+		// Le lien contient un UUID ou ressemble à un slug.
+		const details = await getLinkDetails( pathName );
+
+		if ( details.state && details.data )
+		{
+			// Redirection vers la page de redirection.
+			return NextResponse.redirect( new URL( `/go/${ details.data.id }`, request.nextUrl ) );
+		}
+	}
+
+	// Création du mécanisme de gestion des langues et traductions.
 	//  Source : https://next-intl-docs.vercel.app/docs/getting-started/app-router-server-components
 	const handleI18nRouting = createIntlMiddleware( {
 		locales: getLanguages(),
