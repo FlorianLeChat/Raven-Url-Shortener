3210477d03c2d3b8f42e2fd3ca39f8a31e78f899
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-01-22 18:40:13 +0100

    feat: added client-side route caching for shortcut link details

--- server/src/Action/GetLinkDetailsAction.php
+++ server/src/Action/GetLinkDetailsAction.php
@@ -7,6 +7,7 @@ use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,6 +30,7 @@ final class GetLinkDetailsAction extends AbstractController
 	 * Récupération des informations d'un lien raccourci.
 	 */
 	#[Route("/api/link/{uuid}", methods: ["GET"], stateless: true)]
+	#[Cache(public: true, max_age: 3600, mustRevalidate: true)]
 	public function getLinkDetails(#[MapEntity(id: "uuid")] Link $link): JsonResponse
 	{
 		$this->logger->info(sprintf(LOG_FUNCTION, basename(__FILE__), __NAMESPACE__, __FUNCTION__, __LINE__));
