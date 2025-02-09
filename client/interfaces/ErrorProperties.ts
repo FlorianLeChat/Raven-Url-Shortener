2b89549088cf2775d61864076d507a3d20114cfa
Florian Trayon <git@florian-trayon.fr>
2025-02-09 18:51:13 +0100

    chore: updated types for HTTP error response interfaces from back-end

--- client/interfaces/ErrorProperties.ts
+++ client/interfaces/ErrorProperties.ts
@@ -7,4 +7,15 @@ export interface ErrorProperties {
 
 	// Contenu du message d'erreur.
 	message: string;
+
+	// Liste des erreurs.
+	errors?: {
+		[key: string]: {
+			// Code de l'erreur.
+			code: string;
+
+			// Message de l'erreur.
+			message: string;
+		}[];
+	};
 }
