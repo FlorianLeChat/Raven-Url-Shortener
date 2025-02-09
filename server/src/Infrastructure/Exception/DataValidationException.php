edfbb7e4544675ab89c06b720ad543ad013d8cc6
Florian Trayon <git@florian-trayon.fr>
2025-02-09 18:28:22 +0100

    feat: improved HTTP JSON response format from PHP back-end

---
+++ server/src/Infrastructure/Exception/DataValidationException.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Infrastructure\Exception;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+/**
+ * Exception personnalisée pour les erreurs de validation des données.
+ */
+final class DataValidationException extends HttpException
+{
+	/**
+	 * Liste des erreurs de validation.
+	 */
+	private array $violations;
+
+	/**
+	 * Constructeur de la classe.
+	 */
+	public function __construct(array $violations)
+	{
+		parent::__construct(Response::HTTP_BAD_REQUEST, "An error occurred during data validation.");
+
+		$this->violations = $violations;
+	}
+
+	/**
+	 * Récupère les erreurs de validation.
+	 */
+	public function getViolations(): array
+	{
+		return $this->violations;
+	}
+}
