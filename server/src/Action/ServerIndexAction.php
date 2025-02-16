4325dd0a99ecd4f9adeea1b3d25696243fba9eba
Florian Trayon <git@florian-trayon.fr>
2025-02-16 15:12:57 +0100

    chore: removed Monolog logging on Docker health check route

--- server/src/Action/ServerIndexAction.php
+++ server/src/Action/ServerIndexAction.php
@@ -2,13 +2,10 @@
 
 namespace App\Action;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-use const App\LOG_FUNCTION;
 
 /**
  * Action pour la page d'accueil du serveur.
@@ -16,20 +13,11 @@ use const App\LOG_FUNCTION;
 final class ServerIndexAction extends AbstractController
 {
 	/**
-	 * Constructeur de la classe.
-	 */
-	public function __construct(
-		public readonly LoggerInterface $logger,
-	) {}
-
-	/**
 	 * Récupération de l'état de santé du serveur.
 	 */
 	#[Route("/", methods: ["GET"], stateless: true)]
 	public function getHealthCheck(Request $request): Response
 	{
-		$this->logger->info(sprintf(LOG_FUNCTION, basename(__FILE__), __NAMESPACE__, __FUNCTION__, __LINE__));
-
 		$requestTime = $request->server->get("REQUEST_TIME_FLOAT");
 		$currentTime = microtime(true);
 		$executionTime = round(($currentTime - $requestTime) * 1000, 3);
