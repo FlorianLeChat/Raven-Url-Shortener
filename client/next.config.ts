b19d60a725beb8675e63c6089c8a7ff9caa16517
Florian Trayon <git@florian-trayon.fr>
2025-03-16 17:33:56 +0100

    chore: removed usage of NextJS server as gateway to PHP back-end

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,13 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin( "./utilities/i18n.ts" );
 const nextConfig: NextConfig = withNextIntl( {
 	serverExternalPackages: [ "pino", "pino-pretty" ], // https://github.com/vercel/next.js/discussions/46987#discussioncomment-8464812
-	poweredByHeader: false,
-	rewrites: async () => [
-		{
-			source: "/api/:path*",
-			destination: `${ process.env.NEXT_PUBLIC_BACKEND_URL }/api/:path*`
-		}
-	]
+	poweredByHeader: false
 } );
 
 export default nextConfig;
