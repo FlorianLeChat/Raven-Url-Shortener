703d71f7b2ff4c29b5b11e1f89cbf1c24e71e920
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-03-16 17:32:52 +0100

    fix: data collection from Doctrine entities since latest changes for PHPStan

--- server/src/Domain/Entity/Link.php
+++ server/src/Domain/Entity/Link.php
@@ -172,13 +172,10 @@ class Link
 
 	/**
 	 * Conversion de l'entité en tableau.
-	 * @return array<string, Link>
+	 * @return array<string, mixed>
 	 */
 	public function toArray(): array
 	{
-		return array_filter(
-			get_object_vars($this),
-			fn($value) => $value instanceof Link
-		);
+		return get_object_vars($this);
 	}
 }
