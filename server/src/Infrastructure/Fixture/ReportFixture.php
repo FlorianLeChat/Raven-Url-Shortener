141e230591477ef2c5ecd285ed57e519646307c6
Florian Trayon <git@florian-trayon.fr>
2025-05-13 21:44:48 +0200

    test: added specific classes for PHPUnit test fixtures

---
+++ server/src/Infrastructure/Fixture/ReportFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Infrastructure\Fixture;
+
+use DateTime;
+use App\Domain\Entity\Link;
+use App\Domain\Entity\Report;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+/**
+ * Données de test pour les signalements de liens raccourcis.
+ */
+final class ReportFixture extends Fixture
+{
+	public function load(ObjectManager $manager): void
+	{
+		$link = $this->getReference("link", Link::class);
+
+		$report = new Report();
+		$report->setEmail('johndoe@domain.com');
+		$report->setReason('Spam');
+		$report->setLink($link);
+		$report->setCreatedAt(new DateTime());
+
+		$manager->persist($report);
+		$manager->flush();
+	}
+}
