9578fd484669bc5969c3d2553cd906003e7e430d
Florian Trayon <git@florian-trayon.fr>
2025-05-14 21:38:15 +0200

    test: added application test on shortcut link partial update API routes

---
+++ server/tests/Action/UpdateLinkActionTest.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Tests\Action;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Test d'application pour l'action de mise à jour partielle ou
+ *  complète d'un lien raccourci.
+ */
+final class UpdateLinkActionTest extends WebTestCase
+{
+	/**
+	 * Client de navigation.
+	 */
+	protected KernelBrowser $client;
+
+	/**
+	 * Population de la base de données avec des données fictives.
+	 */
+	protected function setUp(): void
+	{
+		parent::setUp();
+
+		$this->client = static::createClient();
+
+		$php = new PhpExecutableFinder();
+		$process = new Process([
+			$php->find(),
+			sprintf('%s/bin/console', $this->client->getKernel()->getProjectDir()),
+			'doctrine:fixtures:load',
+			'--env=test',
+			'--no-interaction'
+		]);
+
+		$process->disableOutput();
+		$process->run();
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci sans clé API.
+	 */
+	public function testPatchLinkWithMissingApiKey(): void
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d9');
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+
+		$content = $this->client->getResponse()->getContent();
+
+		$this->assertJson($content);
+		$this->assertStringStartsWith('{"code":400,"message":"An error occurred during data validation."', $content);
+		$this->assertStringContainsString('MISSING_API_KEY_ERROR', $content);
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci avec une clé API invalide.
+	 */
+	public function testPatchLinkWithInvalidApiKey(): void
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d9', server: [
+			'HTTP_Authorization' => 'TEST'
+		]);
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+
+		$content = $this->client->getResponse()->getContent();
+
+		$this->assertJson($content);
+		$this->assertJsonStringEqualsJsonString('{"code":403,"message":"The provided API key is invalid. Please check it and try again."}', $content);
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci désactivé par un administrateur.
+	 */
+	public function testPatchDisabledLink()
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d0', server: [
+			'HTTP_Authorization' => 'Bearer f4a93c5d2be6e78a46b3fe190a5c8df1e209b6ce03d84224a80e6f1f5cb34b79'
+		]);
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+
+		$content = $this->client->getResponse()->getContent();
+
+		$this->assertJson($content);
+		$this->assertJsonStringEqualsJsonString('{"code":403,"message":"The specified shortcut link has been disabled by its owner or by an administrator."}', $content);
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci signalé par un utilisateur.
+	 */
+	public function testPatchReportedLink()
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d5', server: [
+			'HTTP_Authorization' => 'Bearer e1d2a8f7944b134de782a4b5f10a0c9b9d3b7f8ee649ce13a6fa2e3d294f3170'
+		]);
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+
+		$content = $this->client->getResponse()->getContent();
+
+		$this->assertJson($content);
+		$this->assertJsonStringEqualsJsonString('{"code":403,"message":"The specified shortcut link has been reported by one or more users. This link must be verified by an administrator before it can be managed again."}', $content);
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci avec des données invalides.
+	 */
+	public function testPatchLinkWithInvalidData(): void
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d9', content: json_encode([
+			'field' => 'url',
+			'value' => 'invalid_url'
+		]), server: [
+			'HTTP_Authorization' => 'Bearer c7e8f5b19f2341b59d7b0a0dc33c3e847a9d4f6ae95e3c738f621a1296bcf207'
+		]);
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+
+		$content = $this->client->getResponse()->getContent();
+
+		$this->assertJson($content);
+		$this->assertStringStartsWith('{"code":400,"message":"An error occurred during data validation."', $content);
+		$this->assertStringContainsString('INVALID_URL_ERROR', $content);
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci avec un slug dupliqué.
+	 */
+	public function testPatchLinkWithDuplicatedSlug(): void
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d9', content: json_encode([
+			'field' => 'slug',
+			'value' => 'test1'
+		]), server: [
+			'HTTP_Authorization' => 'Bearer c7e8f5b19f2341b59d7b0a0dc33c3e847a9d4f6ae95e3c738f621a1296bcf207'
+		]);
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+
+		$content = $this->client->getResponse()->getContent();
+
+		$this->assertJson($content);
+		$this->assertStringStartsWith('{"code":400,"message":"An error occurred during data validation."', $content);
+		$this->assertStringContainsString('DUPLICATE_SLUG_ERROR', $content);
+	}
+
+	/**
+	 * Test d'une mise à jour partielle d'un lien raccourci avec des données valides.
+	 */
+	public function testPatchLinkWithValidData(): void
+	{
+		$this->client->request('PATCH', '/api/v1/link/0196cb17-b0f8-7e9c-b381-ef17aa05f3d9', content: json_encode([
+			'field' => 'url',
+			'value' => 'https://www.example.com'
+		]), server: [
+			'HTTP_Authorization' => 'Bearer c7e8f5b19f2341b59d7b0a0dc33c3e847a9d4f6ae95e3c738f621a1296bcf207'
+		]);
+
+		$this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+		$this->assertJson($this->client->getResponse()->getContent());
+	}
+}
