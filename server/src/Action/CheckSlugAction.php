feec8ef95e22cbe24c39d5deec23fcaa18479d94
Florian Trayon <git@florian-trayon.fr>
2025-05-22 20:53:53 +0200

    feat: added parameter declaration and API route payload for Swagger

--- server/src/Action/CheckSlugAction.php
+++ server/src/Action/CheckSlugAction.php
@@ -3,6 +3,7 @@
 namespace App\Action;
 
 use Psr\Log\LoggerInterface;
+use OpenApi\Attributes as OA;
 use App\Domain\Service\CheckSlugService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,6 +31,22 @@ final class CheckSlugAction extends AbstractController
 	 * Vérification de la disponibilité d'un slug personnalisé.
 	 */
 	#[Route('/slug', methods: ['POST'])]
+	#[OA\Post(
+		tags: ['Slug'],
+		summary: 'Check slug availability',
+		description: 'Check if a custom slug is available.',
+		parameters: [
+			new OA\Parameter(name: 'version', in: 'path', description: 'The API version to use.', schema: new OA\Schema(type: 'string')),
+			new OA\RequestBody(content: new OA\JsonContent(type: 'object', properties: [
+				new OA\Property(property: 'slug', type: 'string', description: 'The slug to check for availability.'),
+			]))
+		],
+		responses: [
+			new OA\Response(response: JsonResponse::HTTP_OK, description: 'Slug is available or not', content: new OA\JsonContent(type: 'object', properties: [
+				new OA\Property(property: 'available', type: 'boolean', description: 'True if the slug is available, false otherwise.'),
+			]))
+		],
+	)]
 	public function checkSlug(Request $request): JsonResponse
 	{
 		$this->logger->info(sprintf(LOG_FUNCTION, basename(__FILE__), __NAMESPACE__, __FUNCTION__, __LINE__));
--- server/src/Action/DeleteLinkAction.php
+++ server/src/Action/DeleteLinkAction.php
@@ -4,8 +4,10 @@ namespace App\Action;
 
 use App\Domain\Entity\Link;
 use Psr\Log\LoggerInterface;
+use OpenApi\Attributes as OA;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Domain\Service\DeleteLinkService;
+use Nelmio\ApiDocBundle\Attribute\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -39,6 +41,35 @@ final class DeleteLinkAction extends AbstractController
 	 */
 	#[Route('/link/{id}', methods: ['DELETE'], requirements: ['id' => Requirement::UUID_V7])]
 	#[Route('/link/{slug}', methods: ['DELETE'], requirements: ['slug' => Requirement::ASCII_SLUG])]
+	#[Security(name: 'ApiKeyAuth')]
+	#[OA\Delete(
+		tags: ['Link'],
+		summary: 'Delete a short link',
+		description: 'Delete a short link by its UUID or its custom slug.',
+		parameters: [
+			new OA\Parameter(name: 'version', in: 'path', description: 'The API version to use.', schema: new OA\Schema(type: 'string')),
+			new OA\Parameter(name: 'id', in: 'path', description: 'The UUID of the link to delete.', schema: new OA\Schema(type: 'string', format: 'uuid')),
+			new OA\Parameter(name: 'slug', in: 'path', description: 'The custom slug of the link to delete.', schema: new OA\Schema(type: 'string'))
+		],
+		responses: [
+			new OA\Response(response: Response::HTTP_NO_CONTENT, description: 'Link deleted successfully'),
+			new OA\Response(response: Response::HTTP_BAD_REQUEST, description: 'Link data deletion failed', content: new OA\JsonContent(
+				ref: '#/components/schemas/HttpBadRequest'
+			)),
+			new OA\Response(response: Response::HTTP_FORBIDDEN, description: 'Link deletion forbidden', content: new OA\JsonContent(
+				ref: '#/components/schemas/HttpForbidden'
+			)),
+			new OA\Response(response: Response::HTTP_NOT_FOUND, description: 'Link not found', content: new OA\JsonContent(
+				ref: '#/components/schemas/HttpNotFound'
+			)),
+			new OA\Response(response: Response::HTTP_TOO_MANY_REQUESTS, description: 'Link deletion rate limit exceeded', content: new OA\JsonContent(
+				ref: '#/components/schemas/HttpTooManyRequests'
+			)),
+			new OA\Response(response: Response::HTTP_INTERNAL_SERVER_ERROR, description: 'Internal server error', content: new OA\JsonContent(
+				ref: '#/components/schemas/HttpInternalServerError'
+			))
+		]
+	)]
 	public function deleteLink(Request $request, Link $link): Response
 	{
 		$this->logger->info(sprintf(LOG_FUNCTION, basename(__FILE__), __NAMESPACE__, __FUNCTION__, __LINE__));
