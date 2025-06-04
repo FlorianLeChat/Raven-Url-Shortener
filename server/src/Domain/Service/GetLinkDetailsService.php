0509bbccf36703936665237d6221019f9d0f2b65
Florian Trayon <git@florian-trayon.fr>
2025-06-04 18:13:01 +0200

    refactor: moved last visit date update to database on API side

---
+++ server/src/Domain/Service/GetLinkDetailsService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Domain\Service;
+
+use DateTimeImmutable;
+use App\Domain\Entity\Link;
+use App\Domain\Service\Abstract\BaseLinkService;
+
+use const App\LOG_FUNCTION;
+
+/**
+ * Service de récupération des informations d'un lien raccourci.
+ */
+final class GetLinkDetailsService extends BaseLinkService
+{
+	/**
+	 * Récupération des informations d'un lien raccourci.
+	 */
+	public function getLinkDetails(Link $link): Link
+	{
+		$this->logger->info(sprintf(LOG_FUNCTION, basename(__FILE__), __NAMESPACE__, __FUNCTION__, __LINE__));
+
+		$link->setVisitedAt(new DateTimeImmutable());
+
+		$this->repository->save($link, true);
+
+		return $link;
+	}
+}
