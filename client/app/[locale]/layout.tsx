9c4e8dd164d3c36d46dd2a425c7ecd39c3c46367
Florian Trayon <git@florian-trayon.fr>
2025-06-06 21:29:30 +0200

    chore: improved background video rendering on low-contrast screens

--- client/app/[locale]/layout.tsx
+++ client/app/[locale]/layout.tsx
@@ -127,7 +127,7 @@ export default async function Layout( {
 					loop
 					muted
 					autoPlay
-					className="fixed -z-10 hidden size-full object-none opacity-20 dark:block"
+					className="fixed -z-10 hidden size-full object-none opacity-25 dark:block"
 				>
 					<source
 						src={`${ process.env.__NEXT_ROUTER_BASEPATH }/assets/videos/background.mp4`}
