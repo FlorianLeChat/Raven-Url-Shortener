0ac45c9421bbef84babe29fb57f43698d9242881
Florian Trayon <git@florian-trayon.fr>
2025-06-08 18:58:46 +0200

    feat: added a user reporting limit per shortcut link to prevent abuse

--- server/locales/messages.fr.php
+++ server/locales/messages.fr.php
@@ -7,6 +7,7 @@ return [
 	'link.reported' => 'Le lien raccourci spécifié a été signalé par un ou plusieurs utilisateurs. Ce lien doit être vérifié par un administrateur pour être de nouveau accessible.',
 	'report.duplicated' => 'Vous avez déjà signalé ce lien raccourci, vous ne pouvez pas le signaler à nouveau.',
 	'report.trusted_link' => 'Vous ne pouvez pas signaler un lien de confiance. Si vous pensez que ce lien est malveillant, veuillez contacter un administrateur.',
+	'report.maximum_reached' => 'Le nombre maximum de signalements pour ce lien a été atteint (%max%). Il ne peut pas être signalé à nouveau.',
 	'slug.already_used' => 'Le slug personnalisé que vous avez choisi est déjà utilisé par un autre lien. Veuillez en choisir un autre.',
 	'api_key.missing' => 'La clé API est manquante. Veuillez la fournir dans l\'en-tête de la requête HTTP.',
 	'api_key.invalid' => 'La clé API fournie est invalide. Veuillez la vérifier et réessayer.'
