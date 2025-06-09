e789defcb4174f78f7a097916b100d48a924d6fc
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-06-09 20:51:30 +0200

    refactor: moved legal notice consent to new modal provider

--- client/interfaces/ModalProperties.ts
+++ client/interfaces/ModalProperties.ts
@@ -1,24 +1,39 @@
 //
 // Interface des propriétés pour les informations du serveur.
 //
-import type { ReactElement } from "react";
+import type { ReactNode } from "react";
 
 export interface ModalOptionProps
 {
 	// Titre de la boite de dialogue.
-	title: ReactElement;
+	title: string;
 
 	// Contenu de la boite de dialogue.
-	body: ReactElement;
+	body: ReactNode | string;
+
+	// Icône à afficher dans la boite de dialogue.
+	icon: ReactNode;
 
 	// Taille de la boite de dialogue.
 	size?: string;
 
+	// Fonction appelée lors de l'ouverture de la boite de dialogue.
+	onOpen?: () => void;
+
+	// Fonction appelée lors de la fermeture de la boite de dialogue.
+	onClose?: () => void;
+
 	// Texte du bouton de confirmation.
 	confirmText?: string;
 
+	// Type du bouton de confirmation.
+	confirmColor?: string;
+
 	// Texte du bouton d'annulation.
 	cancelText?: string;
+
+	// Type du bouton d'annulation.
+	cancelColor?: string;
 
 	// Indique si la boite de dialogue peut être fermée
 	//  en cliquant en dehors de celle-ci ou via le clavier.
@@ -41,4 +56,7 @@ export interface ModalProviderProps
 {
 	// Fonction d'ouverture de la boite de dialogue.
 	showModal: ( options: ModalOptionProps ) => Promise<ModalResultProps>;
+
+	// Fonction de fermeture de la boite de dialogue.
+	closeModal: () => void;
 }
