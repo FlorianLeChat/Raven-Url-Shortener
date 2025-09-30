0ed202fb8ce17cc000bacd5554c6aa9f2e9bb489
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2025-09-30 20:25:09 +0200

    feat: removed `supercronic` for scheduled tasks with Debian

--- server/docker/scripts/development/entrypoint.sh
+++ server/docker/scripts/development/entrypoint.sh
@@ -7,9 +7,6 @@ composer install
 /usr/local/bin/php bin/console doctrine:database:create --no-interaction --if-not-exists
 /usr/local/bin/php bin/console doctrine:schema:update --force
 
-# Run cron service in background
-supercronic /etc/crontabs/www-data &
-
 # Clear previous Symfony local server cache
 rm -rf ~/.symfony5/
 
